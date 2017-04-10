b8386fc11ebc92de825fdcfa1add000e0bd85ad4
mineichen <kusi.ineichen@gmail.com>
2017-04-10 10:39:38 +0200

    Move Services to Factory Class

---
+++ website/src/Factory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace mineichen;
+
+class Factory
+{
+	private $config;
+	public function __construct(array $config)
+	{
+		$this->config = $config;
+	}
+	
+	public function getIndexController()
+	{
+		return new Controller\IndexController(
+			$this->getTemplateEngine()		
+		);
+	}
+	
+	public function getLoginController()
+	{
+		return new Controller\LoginController(
+			$this->getTemplateEngine(),
+			$this->getLoginService()
+		);
+	}
+	
+	public function getTemplateEngine()
+	{
+		return new SimpleTemplateEngine(__DIR__ . "/../templates/");
+	}
+	
+	public function getPdo()
+	{
+		return new \PDO(
+			"mysql:host=" . $this->config["database"]["host"] . ";dbname=app;charset=utf8",
+			$this->config["database"]["user"],
+			$this->config["database"]["password"],
+			[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+		);
+	}
+	
+	public function getLoginService()
+	{
+		return new Service\LoginMysqlService(
+			$this->getPdo()		
+		);
+	}
+}
--- website/web/index.php
+++ website/web/index.php
@@ -7,21 +7,18 @@ session_start();
 
 
 require_once("../vendor/autoload.php");
-$tmpl = new mineichen\SimpleTemplateEngine(__DIR__ . "/../templates/");
-$pdo = new \PDO(
-	"mysql:host=mariadb;dbname=app;charset=utf8",
-	"root",
-	"my-secret-pw",
-	[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
-$loginService = new LoginMysqlService($pdo);
+$conf = parse_ini_file(__DIR__ . "/../config.ini", true);
+
+$factory = new mineichen\Factory();
+
+
 
 switch($_SERVER["REQUEST_URI"]) {
 	case "/":
-		(new mineichen\Controller\IndexController($tmpl))->homepage();
+		$factory->getIndexController()->homepage();
 		break;
 	case "/login":
-		$ctr = new mineichen\Controller\LoginController($tmpl, $loginService);
+		$ctr = $factory->getLoginController();
 		if($_SERVER["REQUEST_METHOD"] == "GET") {
 			$ctr->showLogin();
 		} else {
@@ -31,7 +28,7 @@ switch($_SERVER["REQUEST_URI"]) {
 	default:
 		$matches = [];
 		if(preg_match("|^/hello/(.+)$|", $_SERVER["REQUEST_URI"], $matches)) {
-			(new mineichen\Controller\IndexController($tmpl))->greet($matches[1]);
+			$factory->getIndexController()->greet($matches[1]);
 			break;
 		}
 		echo "Not Found";
